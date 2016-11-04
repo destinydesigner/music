9abaf8a39aa5754094400cec77e02af2195391ce
Fang Guojian <fanggj@garena.com>
2016-11-04 13:04:18 +0800

    only accept json object

--- request.py
+++ request.py
@@ -5,6 +5,8 @@ class Request(object):
     def __init__(self, data):
         try:
             self.data = json.loads(data)
+            if not isinstance(self.data, dict):
+                raise Exception("Please send a json object.")
         except Exception as e:
             self.data = {
                 'cmd': -1,
